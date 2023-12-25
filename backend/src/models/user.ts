90a65d574660b094b2aa5526f7e4425f601bf1cf
brian-ambani <brainambani1@gmail.com>
2023-12-26 02:18:47 +0300

    added password encyrprion with bcrpt

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 export type Usertype = {
   _id: string;
@@ -10,9 +11,16 @@ export type Usertype = {
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
-  password: { type: String, Required: true },
+  password: { type: String, required: true },
   firstName: { type: String, Required: true },
   lastName: { type: String, Required: true },
+});
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 9);
+  }
+  next();
 });
 
 const User = mongoose.model<Usertype>("User", userSchema);
