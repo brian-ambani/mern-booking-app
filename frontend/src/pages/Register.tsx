fbeea4d730ef294347de2486dfc840e2083504d8
brian-ambani <brainambani1@gmail.com>
2023-12-27 02:39:36 +0300

    updated navigate to home page after successful login

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -2,6 +2,7 @@ import { useForm } from "react-hook-form";
 import { useMutation } from "react-query";
 import * as apiClient from "../api-client";
 import { useAppContext } from "../contexts/AppContext";
+import { useNavigate } from "react-router-dom";
 
 export type RegisterFormData = {
   firstName: string;
@@ -12,6 +13,7 @@ export type RegisterFormData = {
 };
 
 const Register = () => {
+  const navigate = useNavigate();
   const { showToast } = useAppContext();
 
   const {
@@ -27,6 +29,7 @@ const Register = () => {
         message: "Registration Success",
         type: "SUCCESS",
       });
+      navigate("/");
     },
     onError: (error: Error) => {
       showToast({
