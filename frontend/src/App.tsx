5aeb0642b8baaa090294ea61f41bf5adbf849804
brian-ambani <brainambani1@gmail.com>
2023-12-26 03:53:13 +0300

    updated the register route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import {
   Navigate,
 } from "react-router-dom";
 import Layout from "./layout/Layout";
+import Register from "./pages/Register";
 
 function App() {
   return (
@@ -26,7 +27,15 @@ function App() {
             </Layout>
           }
         />
-        <Route path="/" element={<Navigate to="/" />} />
+        <Route
+          path="/register"
+          element={
+            <Layout>
+              <Register />
+            </Layout>
+          }
+        />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
   );
