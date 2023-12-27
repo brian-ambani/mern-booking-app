6dfee652a8043e74e7c703c2100ad78085d46f75
brian-ambani <brainambani1@gmail.com>
2023-12-27 03:32:54 +0300

    updated the api-client

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -1,9 +1,11 @@
 import { RegisterFormData } from "./pages/Register";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL as string;
+
 export const register = async (formData: RegisterFormData) => {
   const response = await fetch(`${API_BASE_URL}/api/users/register`, {
     method: "POST",
+    credentials: "include",
     headers: {
       "Content-Type": "application/json",
     },
@@ -15,4 +17,18 @@ export const register = async (formData: RegisterFormData) => {
   if (!response.ok) {
     throw new Error(responseBody.message);
   }
+};
+
+export const validateToken = async () => {
+  const response = await fetch(`${API_BASE_URL}/api/auth/validate-token`, {
+    credentials: "include",
+  });
+
+  const responseBody = await response.json();
+
+  if (!response.ok) {
+    throw new Error(responseBody.message);
+  }
+
+  return responseBody;
 };
