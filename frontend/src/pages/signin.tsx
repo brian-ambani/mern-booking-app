d5ca23f1e4cebd77487e3d56aaa04edd8beca079
brian-ambani <brainambani1@gmail.com>
2024-01-01 03:01:35 +0300

    updated the queryclient on the sign in page

--- frontend/src/pages/signin.tsx
+++ frontend/src/pages/signin.tsx
@@ -1,5 +1,5 @@
 import { useForm } from "react-hook-form";
-import { useMutation } from "react-query";
+import { useMutation, useQueryClient } from "react-query";
 import * as apiClient from "../api-client";
 import { useAppContext } from "../contexts/AppContext";
 import { useNavigate } from "react-router-dom";
@@ -10,6 +10,8 @@ export type SignInFormData = {
 };
 
 const SignIn = () => {
+  const queryClient = useQueryClient();
+
   const { showToast } = useAppContext();
   const navigate = useNavigate();
 
@@ -25,6 +27,7 @@ const SignIn = () => {
         message: "Sign In Successfull!",
         type: "SUCCESS",
       });
+      await queryClient.invalidateQueries("validateToken");
       navigate("/");
     },
     onError: (error: Error) => {
